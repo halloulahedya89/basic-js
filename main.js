fbbed862ca0aac41b0785f4b2ca87c14a8b7973f
halloulahedya89 <48623387+halloulahedya89@users.noreply.github.com>
2019-04-06 15:32:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+ function Bold ()
+                {document.getElementById('txtArea').style.fontWeight='bold';}
+
+                 
+                 
+ function Italic ()
+                {document.getElementById('txtArea').style.fontStyle='italic';}
+        
+
+ function Underline ()
+                {document.getElementById('txtArea').style.textDecoration='underline';}
+        
+
+                
+function myFunction(fontlist) {
+  document.getElementById("txtArea").style.fontFamily=fontlist.value;}
+  
+  
+function siz(sizze){
+document.getElementById("txtArea").style.fontSize=sizze.value;}
+
+
+  //if(document.getElementById("a").style.fontweight=="normal") else ...=normal//
+
+  
+
+
